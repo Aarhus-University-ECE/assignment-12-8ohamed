117450dc02acde4ce99548829fc5bd04010dc78f
Muhamed <kar17mo@gmail.com>
2022-11-24 20:18:28 +0100

    u

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /mnt/c/Users/Mohamed/Desktop/Programmering/12/assignment-12-8ohamed
+# Build directory: /mnt/c/Users/Mohamed/Desktop/Programmering/12/assignment-12-8ohamed/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("tests")
---
+++ build/tests/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /mnt/c/Users/Mohamed/Desktop/Programmering/12/assignment-12-8ohamed/tests
+# Build directory: /mnt/c/Users/Mohamed/Desktop/Programmering/12/assignment-12-8ohamed/build/tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(usage_test "tests")
+set_tests_properties(usage_test PROPERTIES  _BACKTRACE_TRIPLES "/mnt/c/Users/Mohamed/Desktop/Programmering/12/assignment-12-8ohamed/tests/CMakeLists.txt;6;add_test;/mnt/c/Users/Mohamed/Desktop/Programmering/12/assignment-12-8ohamed/tests/CMakeLists.txt;0;")
